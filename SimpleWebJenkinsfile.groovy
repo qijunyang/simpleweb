8e310f8b9724189cad1e286cbc33f27c41b9d65a
Qijun Yang <Qijun.Yang@morningstar.com>
2021-05-18 16:13:42 -0500

    add jenkins pipline

--- SimpleWebJenkinsfile.groovy
+++ SimpleWebJenkinsfile.groovy
@@ -3,7 +3,8 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                sh 'echo 123'
+                sh 'npm -i -g yarn'
+                sh 'yarn test'
             }
         }
     }
